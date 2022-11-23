aae66088ce1a6a7110de543a04bc047cc7973385
gladiateurx <borgyeb@gmail.com>
2022-11-23 14:39:39 +0100

    initial commit, set up vite react js project

---
+++ src/App.jsx
@@ -0,0 +1,34 @@
+import './App.css'
+
+const fetchMovies = async () => {
+  const data = await fetch('https://tmdb.sandbox.zoosh.ie/dev/grphql', {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({
+      query: `{
+        movies: popularMovies {
+          name
+          score
+          genres {
+            name
+          }
+        }
+      }`,
+    }),
+  })
+  const popularMovies = await data.json()
+  return popularMovies
+}
+
+console.log(fetchMovies())
+
+const App = () => {
+  return (
+    <div className='app'>
+      <h1>Hello, Vite project!</h1>
+      <button>Load movies!</button>
+    </div>
+  )
+}
+
+export default App
